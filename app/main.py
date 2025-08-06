7516c8bfde1a0dfa9105f62e9134c12171987146
Akshay Sirsat <sirsatakshay20@gmail.com>
2025-08-07 01:42:19 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def health():
-    return "App is running & healthy!", 200
+@app.route("/")
+def home():
+    return """<pre>
+🟢 Status: Healthy  
+📦 Service: Flask Web API  
+🔁 Environment: Docker + Compose  
+✅ All systems operational
+</pre>"""
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
